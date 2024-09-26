3eb9bfbcd101110e5afcae2b3fe1241df1e9797b
asaregh <140358255+asaregh@users.noreply.github.com>
2024-09-26 09:11:10 +0100

    Delete javascript directory

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-function filterSelection(category) {
-    let products = document.getElementsByClassName('product-card');
-    if (category === 'all') category = '';
-    for (let i = 0; i < products.length; i++) {
-        products[i].style.display = products[i].className.includes(category) ? 'block' : 'none';
-    }
-}
